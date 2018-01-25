78c8c2af350d96ac17e8ae16b761aeb264d5b1a6
Jim Young <jimy@jimy.org>
2018-01-25 16:31:10 -0500

    less verbose log messages

--- config/dev.exs
+++ config/dev.exs
@@ -44,7 +44,7 @@ config :moola, Moola.BinanceSocket,
 
 # Do not include metadata nor timestamps in development logs
 # config :logger, :console, format: "[$level] $message\n"
-config :logger, :console, format: "[$level] $message\n" #, level: :info
+config :logger, :console, format: "[$level] $message\n" , level: :info
 
 # Set a higher stacktrace during development. Avoid configuring such
 # in production as building large stacktraces may be expensive.
